4994e3afb3f4e40d6d31419ec4ff0228fa6ada25
Tim Rooke <timrooke1991@gmail.com>
2021-11-30 08:00:44 +0000

    Merge sort

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,34 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length === 1) {
+    return nums;
+  }
+
+  const midpoint = Math.ceil(nums.length / 2);
+  const leftArray = nums.splice(0, midpoint);
+  const rightArray = nums;
+
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+};
+
+const merge = (leftArray, rightArray) => {
+  const results = [];
+
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] <= rightArray[0]) {
+      results.push(leftArray.shift());
+    } else {
+      results.push(rightArray.shift());
+    }
+  }
+
+  return results.concat(leftArray, rightArray);
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test.only("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
