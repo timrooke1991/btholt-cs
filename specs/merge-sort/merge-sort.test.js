c67d971f0fe58595262e6ba5145ff475c1b34e40
Tim Rooke <timrooke1991@gmail.com>
2021-11-30 08:29:26 +0000

    Additional quickSort comments

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,17 +7,20 @@
 */
 
 const mergeSort = (nums) => {
-  if (nums.length === 1) {
+  // base case
+  if (nums.length < 2) {
     return nums;
   }
 
+  // Break the array up
   const midpoint = Math.ceil(nums.length / 2);
-  const leftArray = nums.splice(0, midpoint);
-  const rightArray = nums;
+  const leftArray = nums.slice(0, midpoint);
+  const rightArray = nums.slice(midpoint);
 
   return merge(mergeSort(leftArray), mergeSort(rightArray));
 };
 
+// Left and right arrays enter already sorted!
 const merge = (leftArray, rightArray) => {
   const results = [];
 
@@ -34,7 +37,7 @@ const merge = (leftArray, rightArray) => {
 
 // unit tests
 // do not modify the below code
-test.only("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
