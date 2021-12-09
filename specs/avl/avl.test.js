70f61ee17850fa054f8440434b9f9b67a20e980e
Tim Rooke <timrooke1991@gmail.com>
2021-12-09 08:42:51 +0000

    AVL Tree implementation

--- specs/avl/avl.test.js
+++ specs/avl/avl.test.js
@@ -49,34 +49,106 @@ class Node {
 
   add(value) {
     // decide to go left or right
+    if (value < this.value) {
+      // go left
+
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      // go right
+
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.right.height > this.left.height) {
+        this.height = this.right.height + 1;
+      }
+    }
 
     this.balance();
   }
 
   balance() {
-    // ask is this node out of balance
-    // if not out of balance, do nothing
-    // if it is out of balance, do I need to single or double rotate
-    // if single, just call rotate on self
-    // if double, call rotate on child then on self
+    const rightHeight = this.right ? this.right.height : 0;
+    const leftHeight = this.left ? this.left.height : 0;
+
+    if (leftHeight > rightHeight + 1) {
+      const leftRightHeight = this.left.right ? this.left.right.height : 0;
+      const leftLeftHeight = this.left.left ? this.left.left.height : 0;
+
+      // double rotation
+      if (leftRightHeight > leftLeftHeight) {
+        this.left.rotateRR();
+      }
+
+      // single rotation
+      this.rotateLL();
+    } else if (rightHeight > leftHeight + 1) {
+      const rightRightHeight = this.right.right ? this.right.right.height : 0;
+      const rightLeftHeight = this.right.left ? this.right.left.height : 0;
+
+      // double rotation
+      if (rightLeftHeight > rightRightHeight) {
+        this.right.rotateLL();
+      }
+
+      // single rotation
+      this.rotateRR();
+    }
   }
 
   rotateRR() {
-    this.right.updateInNewLocation();
-    this.updateInNewLocation();
-  }
-
-  rotateLL() {
+    const valueBefore = this.value;
+    const leftBefore = this.left;
+    this.value = this.right.value;
+    this.left = this.right;
+    this.right = this.right.right;
+    this.left.right = this.left.left;
+    this.left.left = leftBefore;
+    this.left.value = valueBefore;
     this.left.updateInNewLocation();
     this.updateInNewLocation();
   }
 
-  updateInNewLocation() {}
+  rotateLL() {
+    const valueBefore = this.value;
+    const rightBefore = this.right;
+    this.value = this.left.value;
+    this.right = this.left;
+    this.left = this.left.left;
+    this.right.left = this.right.right;
+    this.right.right = rightBefore;
+    this.right.value = valueBefore;
+    this.right.updateInNewLocation();
+    this.updateInNewLocation();
+  }
+
+  updateInNewLocation() {
+    if (!this.right && !this.left) {
+      this.height = 1;
+    } else if (
+      !this.right ||
+      (this.left && this.right.height < this.left.height)
+    ) {
+      this.height = this.left.height + 1;
+    } else {
+      //if (!this.left || this.right.height > this.left.height)
+      this.height = this.right.height + 1;
+    }
+  }
 }
 
 // unit tests
 // do not modify the below code
-describe.skip("AVL Tree", function () {
+describe("AVL Tree", function () {
   test("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
